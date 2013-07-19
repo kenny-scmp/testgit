46562011b766272ab8f0b7f1f4784b3f98d08a71
kenny.ma <kenny.ma@scmp.com>
2013-07-19 14:15:23 +0800

    test

---
+++ app/Controller/AttachmentsController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @property Attachment $Attachment
+ */
+ class AttachmentsController extends AppController {
+    public function download($id) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid Attachment'));
+        }
+        $fileObj = $this->Attachment->getFile($id);
+        $this->response->type($fileObj['attachment']['Attachment']['type']);
+        $this->response->file($fileObj['file']->path, array('download' => true, 'name' => $fileObj['attachment']['Attachment']['name']));
+        return $this->response;
+    }
+}
+
+
+
+
+?>
